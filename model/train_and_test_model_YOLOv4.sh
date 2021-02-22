d0dabed18608fbc9acc79d1fdf9b525f1589ce54
Mékéné <64429720+IsmaelMekene@users.noreply.github.com>
2021-02-22 09:12:03 +0100

    Update and rename train_model_YOLOv4.sh to train_and_test_model_YOLOv4.sh

--- model/train_and_test_model_YOLOv4.sh
+++ model/train_and_test_model_YOLOv4.sh
@@ -2,6 +2,8 @@
 #If you get CUDA out of memory adjust subdivisions above!
 #adjust max batches down for shorter training above
 
-
+#TRAIN
 ./darknet detector train /content/drive/MyDrive/veryTrueOcrData.data cfg/custom-yolov4-detector.cfg yolov4.conv.137 -dont_show -map
 
+#TEST (In this cas set 'subdivision' to 1)
+./darknet detector test ./meteor.data ./cfg/custom-yolov4-detector.cfg  ./custom-yolov4-detector_9000.weights
